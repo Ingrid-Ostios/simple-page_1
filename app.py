032092e3645855f63e6ba0522e9b0818e7916a90
ingrid-Ostios <ingridostios3107@gmail.com>
2024-08-26 16:57:26 -0500

    :sparkles: Nuevo proyecto

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for
+
+servidor1 = Flask(__name__)
+
+@servidor1.route('/inicio')
+def home():
+    return render_template('plantilla.html')
+
+if __name__ == '__main__':
+    servidor1.run(debug=True)
+    
